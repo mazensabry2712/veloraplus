0565d66ffac2bf4a0f081104d04b5117c009ba7e
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-19 00:48:48 +0300

    chore: remove Laravel skeleton test helpers

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 /*
@@ -15,7 +14,6 @@ use Tests\TestCase;
 */
 
 pest()->extend(TestCase::class)
- // ->use(RefreshDatabase::class)
     ->in('Feature');
 
 /*
@@ -43,8 +41,3 @@ expect()->extend('toBeOne', function () {
 | global functions to help you to reduce the number of lines of code in your test files.
 |
 */
-
-function something()
-{
-    // ..
-}
