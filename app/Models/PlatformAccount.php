87b1b8dce2bde32d21bbe0d01bd85207fe150f43
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-19 00:33:26 +0300

    feat: add platform account model

---
+++ app/Models/PlatformAccount.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppModels;
+
+use DatabaseFactoriesPlatformAccountFactory;
+use IlluminateDatabaseEloquentAttributesFillable;
+use IlluminateDatabaseEloquentAttributesHidden;
+use IlluminateDatabaseEloquentFactoriesHasFactory;
+use IlluminateFoundationAuthUser as Authenticatable;
+use IlluminateNotificationsNotifiable;
+
+#[Fillable(['name', 'email', 'password', 'status'])]
+#[Hidden(['password', 'remember_token'])]
+class PlatformAccount extends Authenticatable
+{
+    /** @use HasFactory<PlatformAccountFactory> */
+    use HasFactory, Notifiable;
+
+    protected $table = 'platform_accounts';
+
+    /**
+     * Get the model attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+};
