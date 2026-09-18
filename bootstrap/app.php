4985f20ea7030cff3d16814c7a5aff236af03c78
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-19 01:06:42 +0300

    feat: register tenant membership middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'tenant' => InitializeTenantContext::class,
+            'tenant.member' => \App\Http\Middleware\EnsureTenantMembership::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
