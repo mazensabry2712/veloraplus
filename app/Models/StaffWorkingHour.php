94518ba4a9e582c02ff384d5fb7579bc7cec2f82
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-19 15:57:34 +0300

    feat: add staff working hour break relation

--- app/Models/StaffWorkingHour.php
+++ app/Models/StaffWorkingHour.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['staff_id', 'day_of_week', 'starts_at', 'ends_at'])]
 class StaffWorkingHour extends TenantModel
@@ -11,5 +12,10 @@ class StaffWorkingHour extends TenantModel
     public function staff(): BelongsTo
     {
         return $this->belongsTo(Staff::class);
+    }
+
+    public function breaks(): HasMany
+    {
+        return $this->hasMany(StaffBreak::class);
     }
 }
