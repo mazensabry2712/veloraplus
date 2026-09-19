81e4e22aec8c1c859123c41a0589d81b161d1adb
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-19 16:23:58 +0300

    feat: wire platform SEO routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
+use App\Application\SEO\SeoManager;
+use App\Http\Controllers\Public\RobotsController;
+use App\Http\Controllers\Public\SitemapController;
 use App\Http\Controllers\Webhooks\KashierWebhookController;
 use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function (SeoManager $seo) {
+    return view('welcome', ['seo' => $seo->platformHome()]);
+})->name('platform.home');
+
+Route::get('/robots.txt', RobotsController::class)->name('seo.robots');
+Route::get('/sitemap.xml', SitemapController::class)->name('seo.sitemap');
 
 Route::post('/webhooks/kashier/platform', KashierWebhookController::class)
     ->withoutMiddleware(ValidateCsrfToken::class);
