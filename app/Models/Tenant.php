a015e78f7e3126ce4d5b6c77ddcbf3abcef12cee
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-20 10:22:18 +0300

    Fix Tenant model Eloquent base import

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -6,6 +6,7 @@ use Database\Factories\TenantFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
