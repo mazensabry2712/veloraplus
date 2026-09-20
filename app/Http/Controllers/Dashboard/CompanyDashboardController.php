a0f7c84c0cbe33c354b3fba3551a5a86cf418ad7
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-20 10:45:46 +0300

    Remove unused dashboard request dependency

--- app/Http/Controllers/Dashboard/CompanyDashboardController.php
+++ app/Http/Controllers/Dashboard/CompanyDashboardController.php
@@ -5,12 +5,11 @@ namespace App\Http\Controllers\Dashboard;
 use App\Application\Dashboard\DashboardContextService;
 use App\Domain\Tenancy\TenantContext;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 final class CompanyDashboardController extends Controller
 {
-    public function __invoke(Request $request, TenantContext $tenantContext, DashboardContextService $dashboardContext): View
+    public function __invoke(TenantContext $tenantContext, DashboardContextService $dashboardContext): View
     {
         $tenant = $tenantContext->current();
 
